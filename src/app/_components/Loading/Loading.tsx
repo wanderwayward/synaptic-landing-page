aceee9bf7011293ab445549f7788655b2ee18f79
louderthanme <rubenaguirrelizcano@gmail.com>
2024-06-11 12:38:04 -0600

    Matched loading colour to the indigo palette

--- src/app/_components/Loading/Loading.tsx
+++ src/app/_components/Loading/Loading.tsx
@@ -7,8 +7,9 @@ const Loading = () => (
     display="flex"
     alignItems="center"
     justifyContent="center"
+    height="100vh"
     width={{ base: "100%", md: "100%", lg: "80%", xl: "60%" }}
-    backgroundColor="black"
+    backgroundColor="rgba(44, 62, 80)"
   >
     <Spinner size="xl" />
   </Box>
