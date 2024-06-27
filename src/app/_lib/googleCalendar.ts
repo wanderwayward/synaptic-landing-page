0376485e745bc6d54125f785f1d697243d23181d
louderthanme <rubenaguirrelizcano@gmail.com>
2024-06-27 10:12:43 -0600

    Removed unused mailgun.ts file and updated googleCalendar.ts file

--- src/app/_lib/googleCalendar.ts
+++ src/app/_lib/googleCalendar.ts
@@ -1,14 +1,27 @@
-// src/app/_lib/googleCalendar.ts
 import { google, Auth } from "googleapis";
+import fs from "fs";
+import path from "path";
 
 const SCOPES = ["https://www.googleapis.com/auth/calendar"];
 
 export const calendar = google.calendar("v3");
 
 export const authenticate = (): Auth.GoogleAuth => {
-  const credentials = JSON.parse(
-    process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON!
-  );
+  let credentials;
+  const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS as string;
+
+  try {
+    const filePath = path.resolve(credentialsPath);
+    credentials = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  } catch (error) {
+    console.error(
+      "Failed to read and parse GOOGLE_APPLICATION_CREDENTIALS file",
+      error
+    );
+    throw new Error(
+      "Failed to read and parse GOOGLE_APPLICATION_CREDENTIALS file"
+    );
+  }
 
   const auth = new google.auth.GoogleAuth({
     credentials,
