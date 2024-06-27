60078eb64c697df16e555d5cfdf96042af2860a8
louderthanme <rubenaguirrelizcano@gmail.com>
2024-06-26 18:35:18 -0600

    Changed connecting to google calendar

--- src/app/_lib/googleCalendar.ts
+++ src/app/_lib/googleCalendar.ts
@@ -1,17 +1,17 @@
 // src/app/_lib/googleCalendar.ts
 import { google, Auth } from "googleapis";
-import path from "path";
 
 const SCOPES = ["https://www.googleapis.com/auth/calendar"];
 
 export const calendar = google.calendar("v3");
 
 export const authenticate = (): Auth.GoogleAuth => {
+  const credentials = JSON.parse(
+    process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON!
+  );
+
   const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(
-      process.cwd(),
-      process.env.GOOGLE_APPLICATION_CREDENTIALS!
-    ),
+    credentials,
     scopes: SCOPES,
   });
 
