56b8212ba554b54a470ee9a1da32090ef7f57676
louderthanme <rubenaguirrelizcano@gmail.com>
2024-06-27 10:45:56 -0600

    Changed to specific env variables rather than pointing to a json file

--- src/app/_lib/googleCalendar.ts
+++ src/app/_lib/googleCalendar.ts
@@ -1,27 +1,42 @@
 import { google, Auth } from "googleapis";
-import fs from "fs";
-import path from "path";
 
 const SCOPES = ["https://www.googleapis.com/auth/calendar"];
 
 export const calendar = google.calendar("v3");
 
-export const authenticate = (): Auth.GoogleAuth => {
-  let credentials;
-  const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS as string;
+const requiredEnvVars = [
+  "GOOGLE_TYPE",
+  "GOOGLE_PROJECT_ID",
+  "GOOGLE_PRIVATE_KEY_ID",
+  "GOOGLE_PRIVATE_KEY",
+  "GOOGLE_CLIENT_EMAIL",
+  "GOOGLE_CLIENT_ID",
+  "GOOGLE_AUTH_URI",
+  "GOOGLE_TOKEN_URI",
+  "GOOGLE_AUTH_PROVIDER_X509_CERT_URL",
+  "GOOGLE_CLIENT_X509_CERT_URL",
+];
 
-  try {
-    const filePath = path.resolve(credentialsPath);
-    credentials = JSON.parse(fs.readFileSync(filePath, "utf8"));
-  } catch (error) {
-    console.error(
-      "Failed to read and parse GOOGLE_APPLICATION_CREDENTIALS file",
-      error
-    );
-    throw new Error(
-      "Failed to read and parse GOOGLE_APPLICATION_CREDENTIALS file"
-    );
+requiredEnvVars.forEach((envVar) => {
+  if (!process.env[envVar]) {
+    throw new Error(`Environment variable ${envVar} is missing`);
   }
+});
+
+export const authenticate = (): Auth.GoogleAuth => {
+  const credentials = {
+    type: process.env.GOOGLE_TYPE!,
+    project_id: process.env.GOOGLE_PROJECT_ID!,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID!,
+    private_key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL!,
+    client_id: process.env.GOOGLE_CLIENT_ID!,
+    auth_uri: process.env.GOOGLE_AUTH_URI!,
+    token_uri: process.env.GOOGLE_TOKEN_URI!,
+    auth_provider_x509_cert_url:
+      process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL!,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL!,
+  };
 
   const auth = new google.auth.GoogleAuth({
     credentials,
