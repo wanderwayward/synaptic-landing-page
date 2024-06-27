830bef59eebb834515078c1710503aa570bca3b9
louderthanme <rubenaguirrelizcano@gmail.com>
2024-06-27 16:35:09 -0600

    Trying to redirect

--- src/app/api/auth/initiate/route.ts
+++ src/app/api/auth/initiate/route.ts
@@ -1,7 +1,12 @@
 // src/app/api/auth/initiate/route.ts
 import { NextRequest, NextResponse } from "next/server";
+import { authenticate } from "@/app/_lib/googleCalendar";
 
 export async function GET(req: NextRequest) {
-  console.log("Initiate route accessed"); // Debug log
-  return new NextResponse("Initiate route is working", { status: 200 });
+  const oAuth2Client = authenticate();
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: ["https://www.googleapis.com/auth/calendar"],
+  });
+  return NextResponse.redirect(authUrl);
 }
