da7e2564d8eb31f1975d6d3b138b57ecb54ec60e
louderthanme <rubenaguirrelizcano@gmail.com>
2024-07-16 13:46:36 -0600

    Removed unused code.

--- src/app/api/test-redirect/route.ts
+++
@@ -1,7 +0,0 @@
-// src/app/api/test-redirect/route.ts
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  console.log("Test redirection"); // Debug log
-  return NextResponse.redirect("https://www.google.com");
-}
--- src/app/backgroundemail/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BackgroundemailPage: React.FC = () => {
-  return <div></div>;
-};
-
-export default BackgroundemailPage;
